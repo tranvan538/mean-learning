9df259cb0ddd27eccd8237295b9e3178794e1db3
Dinh Tran <dinh.tran@vexere.com>
2019-07-20 17:10:33 +0700

    Add json web token

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const router = express.Router();
 
@@ -14,6 +15,31 @@ router.post('/signup', async (request, response, next) => {
     console.error(err);
     response.status(500).json({err});
   }
+});
+
+router.post('/login', async (request, response, next) => {
+  const { email, password } = request.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return response.status(401).json({
+      message: 'User not found'
+    });
+  }
+
+  const result = await bcrypt.compare(password, user.password);
+  if (!result) {
+    return response.status(401).json({
+      message: 'Wrong password'
+    })
+  }
+
+  const token = jwt.sign(
+    {email, userId: user._id},
+    'secret_this_should_be_longer',
+    { expiresIn: '1h' }
+  );
+
+  response.status(200).json({token});
 });
 
 module.exports = router;
