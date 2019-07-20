e55c38da08885f129d2921982c3763b8a1e64ecf
Dinh Tran <dinh.tran@vexere.com>
2019-07-20 16:07:56 +0700

    Finish signup screen

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 const router = express.Router();
 
-router.post('/signup', (request, response, next) => {
-
+router.post('/signup', async (request, response, next) => {
+  const { email, password } = request.body;
+  const hash = await bcrypt.hash(password, 10);
+  const user = new User({email, password: hash});
+  try {
+    const result = await user.save();
+    response.status(201).json({message: 'Success', result })
+  } catch (err) {
+    console.error(err);
+    response.status(500).json({err});
+  }
 });
 
 module.exports = router;
