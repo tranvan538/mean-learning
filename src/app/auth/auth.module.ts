262b8a5843a0d8f0aa2d3e995d848f9b0825c2b6
Dinh Tran <dinh.tran@vexere.com>
2019-07-28 17:12:29 +0700

    Separate module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {SignupComponent} from './signup/signup.component';
+import {LoginComponent} from './login/login.component';
+import {AngularMaterialModule} from '../angular-material.module';
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    SignupComponent,
+  ],
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+    FormsModule
+  ]
+})
+export class AuthModule {
+
+}
